c93ca1a4dcfd91adca95897083c11831e9d83019
VericaG <georgievskaverica9@gmail.com>
2025-08-15 20:12:13 +0200

    First Commit

---
+++ src/test/java/mk/ukim/finki/skit/bmicalculatorapp/BmiCalculatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.skit.bmicalculatorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiCalculatorAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
